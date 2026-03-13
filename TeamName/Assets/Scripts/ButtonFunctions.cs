c8d2d8375acb955969e21b9d2f9c596dde6ee93e
HubertBoomStick <wrighttre55@gmail.com>
2026-03-12 23:06:46 -0400

    Adding the completted Demo

---
+++ TeamName/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+  public void resume()
+    {
+        GameManager.instance.stateUnPause();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.stateUnPause();
+    }
+    public void Quit()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+Application.Quit();
+
+#endif
+
+    }
+
+}
