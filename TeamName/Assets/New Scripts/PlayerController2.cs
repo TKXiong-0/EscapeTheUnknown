186aa4a15984ba7737b8978786df87a3521e4c6b
Thou Xiong <TKXiong@student.fullsail.edu>
2026-03-14 22:55:58 -0400

    made some changes to my CharacterPlayer

--- TeamName/Assets/New Scripts/PlayerController2.cs
+++ TeamName/Assets/New Scripts/PlayerController2.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerControler2 : MonoBehaviour, IDamage
+public class PlayerController2 : MonoBehaviour, IDamage
 {
     [Header("--------------- Components ---------------")]
     [SerializeField] CharacterController controller;
     [SerializeField] Animator characterAnimator;
-    [SerializeField] Transform playerCamera; // This should be your CameraPivot
+    [SerializeField] Transform playerCamera;
     [SerializeField] LayerMask ignoreLayer;
 
-    [Header("--------------- Movement Settings ---------------")]
+    [Header("--------------- Player Stats ---------------")]
     [Range(1, 20)][SerializeField] int HP = 10;
     [Range(1, 20)][SerializeField] int speed = 5;
     [Range(1, 10)][SerializeField] int sprintMod = 2;
@@ -17,11 +17,19 @@ public class PlayerControler2 : MonoBehaviour, IDamage
     [Range(1, 5)][SerializeField] int jumpTimeMax = 1;
     [Range(15, 60)][SerializeField] int Gravity = 20;
 
-    [Header("--------------- Camera Smoothing ---------------")]
+    [Header("--------------- Camera Settings ---------------")]
     [SerializeField] float cameraHeight = 1.4f;
     [SerializeField] float cameraNormalZ = 0.0f;
     [SerializeField] float cameraSprintZ = 0.4f;
+    [SerializeField] float cameraAttackZ = 0.7f;
     [SerializeField] float cameraLerpSpeed = 5.0f;
+    [SerializeField] float attackCameraLerpSpeed = 2.5f;
+    [SerializeField] float attackCameraDuration = 0.45f;
+    [SerializeField] Transform cameraPivot;
+    [SerializeField] Transform mainCam;
+    [SerializeField] float cameraCollisionRadius = 0.2f;
+    [SerializeField] float cameraWallPadding = 0.05f;
+    [SerializeField] LayerMask cameraCollisionMask;
 
     [Header("--------------- Melee Settings ---------------")]
     [SerializeField] int meleeDamage = 3;
@@ -29,14 +37,24 @@ public class PlayerControler2 : MonoBehaviour, IDamage
     [SerializeField] float attackRate = 0.6f;
 
     int jumpCount;
-    int HPorigin;
-    float shootTimer;
-    Vector3 MoveDir;
+    int HPorig;
+    bool isAttacking;
+    bool isAttackLocked;
+    Coroutine attackCameraRoutine;
+    float attackTimer;
+
+    Vector3 moveDir;
     Vector3 playerVel;
 
     void Start()
     {
-        HPorigin = HP;
+        HPorig = HP;
+
+        if (cameraPivot != null)
+        {
+            Vector3 pivotPos = cameraPivot.localPosition;
+            cameraPivot.localPosition = new Vector3(pivotPos.x, cameraHeight, pivotPos.z);
+        }
 
         if (GameManager.instance != null && GameManager.instance.playerHPBar != null)
         {
@@ -46,14 +64,17 @@ public class PlayerControler2 : MonoBehaviour, IDamage
 
     void Update()
     {
-        if (GameManager.instance != null && GameManager.instance.isPaused) return;
+        if (GameManager.instance != null && GameManager.instance.isPaused)
+            return;
 
-        MovementLogic();
+        RotatePlayerToCamera();
+        Movement();
+        HandleMelee();
     }
 
-    void MovementLogic()
+    void Movement()
     {
-        shootTimer += Time.deltaTime;
+        attackTimer += Time.deltaTime;
 
         if (controller.isGrounded)
         {
@@ -61,13 +82,18 @@ public class PlayerControler2 : MonoBehaviour, IDamage
             playerVel.y = -2f;
         }
 
-        float vInput = Input.GetAxis("Vertical");
         float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
         bool isSprinting = Input.GetButton("Sprint");
 
-        float currentSpeed = (isSprinting && vInput > 0) ? speed * sprintMod : speed;
-        MoveDir = (hInput * transform.right) + (vInput * transform.forward);
-        controller.Move(MoveDir * currentSpeed * Time.deltaTime);
+        float currentSpeed = speed;
+        if (isSprinting && vInput > 0)
+        {
+            currentSpeed *= sprintMod;
+        }
+
+        moveDir = (hInput * transform.right) + (vInput * transform.forward);
+        controller.Move(moveDir * currentSpeed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && jumpCount < jumpTimeMax)
         {
@@ -84,30 +110,108 @@ public class PlayerControler2 : MonoBehaviour, IDamage
         playerVel.y -= Gravity * Time.deltaTime;
         controller.Move(playerVel * Time.deltaTime);
 
-        if (playerCamera != null)
-        {
-            float targetZ = (vInput > 0 && isSprinting) ? cameraSprintZ : cameraNormalZ;
-            float currentZ = Mathf.Lerp(playerCamera.localPosition.z, targetZ, Time.deltaTime * cameraLerpSpeed);
+        UpdateCamera(vInput, isSprinting);
+        UpdateAnimations(vInput, hInput, isSprinting);
+    }
 
-            playerCamera.localPosition = new Vector3(0, cameraHeight, currentZ);
+    void RotatePlayerToCamera()
+    {
+        if (cameraPivot == null) return;
+
+        Vector3 camEuler = cameraPivot.eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, camEuler.y, 0f);
+    }
+
+    void UpdateCamera(float vInput, bool isSprinting)
+    {
+        if (mainCam == null || cameraPivot == null) return;
+
+        float targetZ = cameraNormalZ;
+        float currentLerpSpeed = cameraLerpSpeed;
+
+        if (isAttacking)
+        {
+            targetZ = cameraAttackZ;
+            currentLerpSpeed = attackCameraLerpSpeed;
+        }
+        else if (vInput > 0f && isSprinting)
+        {
+            targetZ = cameraSprintZ;
         }
 
-        if (characterAnimator != null)
+        Vector3 desiredLocalPos = new Vector3(0f, 0f, targetZ);
+
+        Vector3 pivotWorldPos = cameraPivot.position;
+        Vector3 desiredWorldPos = cameraPivot.TransformPoint(desiredLocalPos);
+
+        Vector3 dir = desiredWorldPos - pivotWorldPos;
+        float distance = dir.magnitude;
+
+        if (distance > 0.01f)
         {
-            float animValue = vInput;
-            if (vInput > 0) animValue = isSprinting ? 1.0f : 0.5f;
-            characterAnimator.SetFloat("Speed", animValue, 0.1f, Time.deltaTime);
+            dir.Normalize();
+
+            RaycastHit hit;
+            if (Physics.SphereCast(
+                pivotWorldPos,
+                cameraCollisionRadius,
+                dir,
+                out hit,
+                distance,
+                cameraCollisionMask,
+                QueryTriggerInteraction.Ignore))
+            {
+                float safeDistance = Mathf.Max(hit.distance - cameraWallPadding, 0f);
+                desiredWorldPos = pivotWorldPos + dir * safeDistance;
+            }
         }
 
-        if (Input.GetButtonDown("Fire1") && shootTimer >= attackRate)
+        Vector3 finalLocalPos = cameraPivot.InverseTransformPoint(desiredWorldPos);
+
+        mainCam.localPosition = Vector3.Lerp(
+            mainCam.localPosition,
+            finalLocalPos,
+            Time.deltaTime * currentLerpSpeed
+        );
+    }
+
+    void UpdateAnimations(float vInput, float hInput, bool isSprinting)
+    {
+        if (characterAnimator == null) return;
+
+        float moveAmount = Mathf.Abs(vInput) + Mathf.Abs(hInput);
+
+        if (moveAmount > 0.1f)
         {
-            Melee();
+            if (vInput > 0)
+                characterAnimator.SetFloat("Speed", isSprinting ? 1.0f : 0.5f, 0.1f, Time.deltaTime);
+            else
+                characterAnimator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+        }
+        else
+        {
+            characterAnimator.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
         }
     }
 
-    void Melee()
+    void HandleMelee()
     {
-        shootTimer = 0;
+        if (Input.GetButtonDown("Fire1") && attackTimer >= attackRate && !isAttackLocked)
+        {
+            MeleeAttack();
+        }
+    }
+
+    void MeleeAttack()
+    {
+        attackTimer = 0f;
+        isAttackLocked = true;
+
+        if (attackCameraRoutine != null)
+        {
+            StopCoroutine(attackCameraRoutine);
+        }
+        attackCameraRoutine = StartCoroutine(AttackCameraOffset());
 
         if (characterAnimator != null)
         {
@@ -116,10 +220,8 @@ public class PlayerControler2 : MonoBehaviour, IDamage
         }
 
         RaycastHit hit;
-        // Raycast ignores the Player layer so you don't hit yourself
-        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, meleeRange, ~ignoreLayer))
+        if (mainCam != null && Physics.Raycast(mainCam.position, mainCam.forward, out hit, meleeRange, ~ignoreLayer))
         {
-            // Look for IDamage on the hit object or its parent (useful for doors/enemies)
             IDamage dmg = hit.collider.GetComponentInParent<IDamage>();
             if (dmg != null)
             {
@@ -128,18 +230,59 @@ public class PlayerControler2 : MonoBehaviour, IDamage
         }
     }
 
+    IEnumerator AttackCameraOffset()
+    {
+        isAttacking = true;
+
+        yield return new WaitForSeconds(attackCameraDuration);
+
+        isAttacking = false;
+        isAttackLocked = false;
+        attackCameraRoutine = null;
+    }
+
     public void takeDamage(int amount)
     {
         HP -= amount;
+        updatePlayerUI();
 
+        if (HP <= 0)
+        {
+            if (GameManager.instance != null)
+            {
+                GameManager.instance.youLose();
+            }
+        }
+    }
+
+    public void updatePlayerUI()
+    {
         if (GameManager.instance != null && GameManager.instance.playerHPBar != null)
         {
-            GameManager.instance.playerHPBar.fillAmount = (float)HP / HPorigin;
+            GameManager.instance.playerHPBar.fillAmount = (float)HP / HPorig;
         }
+    }
 
-        if (HP <= 0 && GameManager.instance != null)
+    public void StartAttackCameraPush()
+    {
+        isAttacking = true;
+    }
+
+    public void EndAttackCameraPush()
+    {
+        isAttacking = false;
+    }
+
+    public void DealMeleeDamage()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(mainCam.position, mainCam.forward, out hit, meleeRange, ~ignoreLayer))
         {
-            GameManager.instance.youLose();
+            IDamage dmg = hit.collider.GetComponentInParent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.takeDamage(meleeDamage);
+            }
         }
     }
 }
