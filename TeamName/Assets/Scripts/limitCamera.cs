d2a77f2e41a42f8cdba09c1bfe61cbff98f8bab0
Thou Xiong <TKXiong@student.fullsail.edu>
2026-03-26 09:29:02 -0400

    alot of files deleted/revert

--- TeamName/Assets/Scripts/limitCamera.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class limitCamera : MonoBehaviour
-{
-    private void LateUpdate()
-    {        
-        transform.position = new Vector3(GameManager.instance.player.transform.position.x, 40 , GameManager.instance.player.transform.position.z);    
-    }
-}
